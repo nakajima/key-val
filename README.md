8be2d6bbe5fc5e727d15e2331df1e8d8715fc3ac
Pat Nakajima <patnakajima@gmail.com>
2009-05-09 06:18:11 -0400

    good enough

---
+++ README.md
@@ -0,0 +1,27 @@
+# val
+
+Easily store and retrieve key/value pairs in `~/.key-valerie`
+
+## USAGE
+
+### Setting a token:
+
+    $ val foo bar
+
+### Getting a token..
+
+    $ val foo
+
+...will return "bar"
+
+### Unsetting a token...
+
+    $ val foo --unset
+
+...will delete the "bar"
+
+### Listing all tokens:
+
+    $ val
+
+That's it and that's all folks.
---
+++ lib/val.rb
@@ -0,0 +1,80 @@
+# Keep track of key/value pairs in a .key-valerie file.
+#
+# USAGE
+#
+# Setting a token:
+#
+#   $ val foo bar
+#
+# Getting a token:
+#
+#   $ val foo
+#
+# will return "bar"
+#
+# Unsetting a token:
+#
+#   $ val foo --unset
+#
+# will delete the "bar"
+#
+# Listing all tokens:
+#
+#   $ val
+#
+# TODO Real options
+require 'yaml'
+
+class KeyValerie
+  def initialize(args)
+    init unless File.exist?(tokens)
+    sym = case
+    when args.empty?            then :list
+    when args.delete('--unset') then :unset
+    else args.length > 1 ? :set : :get
+    end ; send(sym, *args)
+  end
+
+  def get(key)
+    if token = store[key]
+      puts token
+    end
+  end
+
+  def set(key, val)
+    store[key] = val and save(store)
+    puts "set `#{key}` to #{val}"
+  end
+
+  def unset(key)
+    store.delete(key) and save(store)
+    puts "unset `#{key}` token"
+  end
+
+  def list
+    puts "Key/Value Pairs:"
+    store.each do |key, val|
+      puts "- #{key}: #{val}"
+    end
+  end
+
+  private
+
+  def init
+    File.open(tokens, 'w+') { |f| f << '' }
+  end
+
+  def save(new_store)
+    File.open(tokens, 'w+') do |file|
+      YAML.dump(store, file)
+    end
+  end
+
+  def store
+    @store ||= YAML.load_file(tokens) || {}
+  end
+
+  def tokens
+    File.join(ENV['HOME'], '.key-valerie')
+  end
+end
